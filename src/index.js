9c2a96b6f92a3039c41582391e42b10a49101305
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2019-09-24 23:46:36 -0500

    First problem solved. Better use of setTimeout including resolve and reject

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@
 const randomTime = (max) => Math.ceil(Math.random() * max);
 
 const orders = (product, table) => {
-  console.log(`### Orden: ${product} para ${table}`);
+  console.log(`### Orden: ${product} para ${table}\n`);
   return new Promise((resolve, reject) => {
     const time = randomTime(8000);
-    if (time < 7000) {
-      setTimeout(() => {
-        resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-      }, time);
-    } else {
-      reject(new Error(`--- No se puede cumplir el pedido: ${product} de la mesa ${table}. Demora`));
-    }
+    setTimeout(() => {
+      if (time < 7500) {
+        resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}\n`);
+      } else {
+        // simulate case reject when time is long in ms
+        reject(new TypeError(`Demora en la cocina (${time}ms).
+        -- No se pudó cumplir el pedido: ${product} de la ${table}`));
+      }
+    }, time);
   });
 };
 
@@ -28,6 +30,5 @@ const waiter = () => {
     .then((response) => { console.log(response); })
     .catch((error) => { console.warn(error); });
 };
-
 
 waiter();
